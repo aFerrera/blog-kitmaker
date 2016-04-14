d0ef29ebf18373585709ac2e055bb1942021072d
aFerrera <toni.ferrera.m@gmail.com>
2016-04-14 14:54:32 +0200

    3.1    14/04/2016
    arreglos generales

--- application/views/home/about.php
+++ application/views/home/about.php
@@ -8,28 +8,28 @@
      <img src="<?=base_url('assets/img/codeigniter.png')?>">
      <div class="caption left-align">
        <h3>codeigniter</h3>
-       <h5 class="light grey-text text-lighten-3">PHP framework</h5>
+       <h5 class="light grey-text text-lighten-3 black-text">PHP framework</h5>
      </div>
    </li>
    <li>
      <img src="<?=base_url('assets/img/materialize.png')?>">
      <div class="caption right-align">
        <h3>Materialize</h3>
-       <h5 class="light grey-text text-lighten-3">Style framework</h5>
+       <h5 class="light grey-text text-lighten-3 black-text">Style framework</h5>
      </div>
    </li>
    <li>
      <img src="<?=base_url('assets/img/jquery.png')?>">
      <div class="caption left-align">
        <h3></h3>
-       <h5 class="light grey-text text-lighten-3"></h5>
+       <h5 class="light grey-text text-lighten-3 black-text"></h5>
      </div>
    </li>
    <li>
      <img src="<?=base_url('assets/img/html-css.png')?>">
      <div class="caption left-align">
        <h3>html + css</h3>
-       <h5 class="light grey-text text-lighten-3">Basic</h5>
+       <h5 class="light grey-text text-lighten-3 black-text">Basic</h5>
      </div>
    </li>
  </ul>
