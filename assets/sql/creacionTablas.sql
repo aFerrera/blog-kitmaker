dcd8b869cfa2f7365aa6d065d38881b1634811f0
aFerrera <toni.ferrera.m@gmail.com>
2016-04-14 12:25:58 +0200

    version 2.1.1 14/04/2016
    script sql actualizado con los likes

--- assets/sql/creacionTablas.sql
+++ assets/sql/creacionTablas.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-04-2016 a las 10:08:18
+-- Tiempo de generación: 14-04-2016 a las 12:24:41
 -- Versión del servidor: 10.1.10-MariaDB
 -- Versión de PHP: 5.6.19
 
@@ -31,21 +31,24 @@ CREATE TABLE `comentario` (
   `autor` varchar(50) DEFAULT NULL,
   `fecha` date DEFAULT NULL,
   `contenido` text,
-  `noticia` int(11) NOT NULL
+  `noticia` int(11) NOT NULL,
+  `likes` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `comentario`
 --
 
-INSERT INTO `comentario` (`id`, `autor`, `fecha`, `contenido`, `noticia`) VALUES
-(1, 'toni', '2016-04-14', 'Que pasa gente', 4),
-(2, 'toni', '2016-04-14', 'Se me olvidaba, como va el asunto?', 4),
-(3, '', '2016-04-14', 'Va biiieeeeen!', 4),
-(4, 'admin', '2016-04-14', 'Aver que soy el administrador eh, no me cabreeis..', 4),
-(5, 'admin', '2016-04-14', 'Aver con un text area', 4),
-(6, 'admin', '2016-04-14', 'Primero en comentar!! :D', 3),
-(7, '', '2016-04-14', 'Comentario de anonimo', 2);
+INSERT INTO `comentario` (`id`, `autor`, `fecha`, `contenido`, `noticia`, `likes`) VALUES
+(1, 'toni', '2016-04-14', 'Que pasa gente', 4, 1),
+(2, 'toni', '2016-04-14', 'Se me olvidaba, como va el asunto?', 4, 1),
+(3, '', '2016-04-14', 'Va biiieeeeen!', 4, 1),
+(4, 'admin', '2016-04-14', 'Aver que soy el administrador eh, no me cabreeis..', 4, 3),
+(5, 'admin', '2016-04-14', 'Aver con un text area', 4, 2),
+(6, 'admin', '2016-04-14', 'Primero en comentar!! :D', 3, 0),
+(7, '', '2016-04-14', 'Comentario de anonimo', 2, 1),
+(8, 'Anonimo', '2016-04-14', 'Bieeeeen!, comentando como user anonimo.', 5, 1),
+(9, 'Anonimo', '2016-04-14', 'Anonimous comentando!', 6, 4);
 
 -- --------------------------------------------------------
 
@@ -67,7 +70,8 @@ CREATE TABLE `noticia` (
 
 INSERT INTO `noticia` (`id`, `titulo`, `texto`, `fecha`, `autor`) VALUES
 (2, 'POST PRUEBAS', 'esto lo a escrito el usuario admin!', '2016-04-13', 'admin'),
-(4, 'POOOOOOST', 'by toni :D', '2016-04-13', 'toni');
+(4, 'POOOOOOST', 'by toni :D', '2016-04-13', 'toni'),
+(6, 'NUEVA COMPROBACION RETURNO', 'Aver si va bien esto', '2016-04-14', 'Anonimo');
 
 -- --------------------------------------------------------
 
@@ -89,7 +93,8 @@ CREATE TABLE `usuario` (
 INSERT INTO `usuario` (`id`, `user`, `password`, `admin`) VALUES
 (1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 1),
 (2, 'toni', '81dc9bdb52d04dc20036dbd8313ed055', 0),
-(3, 'jacob', '81dc9bdb52d04dc20036dbd8313ed055', 0);
+(3, 'jacob', '81dc9bdb52d04dc20036dbd8313ed055', 0),
+(4, 'tupac', '81dc9bdb52d04dc20036dbd8313ed055', 0);
 
 --
 -- Índices para tablas volcadas
@@ -121,17 +126,17 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `comentario`
 --
 ALTER TABLE `comentario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT de la tabla `noticia`
 --
 ALTER TABLE `noticia`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
